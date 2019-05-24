fa5050d14769abd46ec60722f81d40cd1f483d13
Saurav Adhikari <sauravads123@gmail.com>
2019-05-24 14:02:56 +0545

    Fix Merge Conflict

--- client/src/container/QuizModule/QuizContainer/Quiz.js
+++ client/src/container/QuizModule/QuizContainer/Quiz.js
@@ -1,20 +1,10 @@
 import React from "react";
-<<<<<<< HEAD
 import axios from "axios";
 import QuizQuestion from "../../../components/QuizModule/QuizQuestion/QuizQuestion";
 
 class Quiz extends React.Component {
   state = {
     questions: null,
-=======
-import QuizQuestion from "../../components/QuizQuestion/QuizQuestion";
-import CountDownTimer from "../../components/Countdown/CountDownTImer";
-import axios from "axios";
-
-class Quiz extends React.Component {
-  state = {
-    questions: [],
->>>>>>> b643e446898d1b88f7132faa9542d65129ce3c68
     score: 0,
     nextQuestion: 0
   };
@@ -27,10 +17,6 @@ class Quiz extends React.Component {
       })
       .catch(error => console.log(error));
   }
-<<<<<<< HEAD
-
-=======
->>>>>>> b643e446898d1b88f7132faa9542d65129ce3c68
   clickHandler = e => {
     let score = this.state.score;
     let nextQuestion = this.state.nextQuestion;
@@ -49,30 +35,9 @@ class Quiz extends React.Component {
     }, 1000);
   };
   render() {
-<<<<<<< HEAD
     let loadedQuestion = <p>Loading Questions</p>;
     if (this.state.questions !== null) {
       loadedQuestion = this.state.questions[0];
-=======
-    let quiz;
-    if (this.state.nextQuestion < this.state.questionList.length) {
-      quiz = (
-        <QuizQuestion
-          question={this.state.questions}
-          clicked={this.clickHandler}
-          question={this.state.questionList[this.state.nextQuestion]}
-        />
-      );
-    } else {
-      quiz = (
-        <div
-          className="container-fluid bg-primary align-items-center"
-          style={{ height: "100vh", width: "100vw" }}
-        >
-          <h1 className="text-center">Thank You for Your Participation</h1>
-        </div>
-      );
->>>>>>> b643e446898d1b88f7132faa9542d65129ce3c68
     }
     return (
       <div className="container">
