551771f09513e072f825951c780713c29fc1b468
beevekmgr <beevekmgr@gmail.com>
2019-05-24 15:04:22 +0545

    Router in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
+import "./App.css";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
 import Quiz from "./container/QuizModule/QuizContainer/Quiz";
+import ChallengeItem from "./components/ChallengeModule/ChallengeItem/ChallengeItem";
+import Navigation from "./container/Navigation/Navigation";
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Quiz />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navigation />
+          <div>
+            <Route path="/" component={SignIn} exact />
+            <Route path="/Signup" component={SignUp} />
+            <Route path="/Quiz" component={Quiz} />
+            <Route path="/Challenge" component={ChallengeItem} />
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
