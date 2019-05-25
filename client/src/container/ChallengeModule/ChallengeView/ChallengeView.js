e249c5059f60c7845270ee7c63df235a3b6093b2
Saurav Adhikari <sauravads123@gmail.com>
2019-05-25 15:05:45 +0545

    Implement GET /api/challenges/resources:/id

--- client/src/container/ChallengeModule/ChallengeView/ChallengeView.js
+++ client/src/container/ChallengeModule/ChallengeView/ChallengeView.js
@@ -4,30 +4,30 @@ class ChallengeView extends React.Component {
     return (
       <div>
         <ul className="nav nav-tabs">
-          <li className="list-item">
-            <a data-toggle="tab" href="#">
+          <li className="nav-item">
+            <a data-toggle="tab" href="#instructions" className="nav-link">
               Instructions
             </a>
           </li>
-          <li className="list-item">
-            <a data-toggle="tab" href="#">
+          <li className="nav-item">
+            <a data-toggle="tab" href="#code" className="nav-link">
               Code
             </a>
           </li>
-          <li className="list-item">
-            <a data-toggle="tab" href="#">
+          <li className="nav-item">
+            <a data-toggle="tab" href="#resources" className="nav-link">
               Resources
             </a>
           </li>
         </ul>
-        <div class="tab-content">
-          <div id="instructions" class="tab-pane fade in active">
+        <div class="tab-content bg-success">
+          <div id="instructions" class="tab-pane fade in active  bg-danger">
             <h3>Instructions</h3>
           </div>
-          <div id="menu1" class="tab-pane fade">
+          <div id="code" class="tab-pane fade  bg-light">
             <h3>Code</h3>
           </div>
-          <div id="menu2" class="tab-pane fade">
+          <div id="resources" class="tab-pane fade  bg-dark">
             <h3>Resources</h3>
           </div>
         </div>
