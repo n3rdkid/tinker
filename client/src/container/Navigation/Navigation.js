46547ead152db13f8cccb85fadcae56366f1cfef
beevekmgr <beevekmgr@gmail.com>
2019-05-26 15:08:41 +0545

    Navigation bar redesign(Bootstrap nav added)

--- client/src/container/Navigation/Navigation.js
+++ client/src/container/Navigation/Navigation.js
@@ -1,27 +1,43 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Navigation = () => {
   return (
     <div>
-      <Container>
-        <Row>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/Quiz">Quiz</NavLink>
-          <NavLink to="/Challenges">Challenges</NavLink>
-          <NavLink to="/AddChallenge">Add Challenge</NavLink>
-          <NavLink to="/AddResources">Add Resources</NavLink>
-          <NavLink to="/AddAssignment">Add Assignment</NavLink>
-          <DropdownButton id="dropdown-basic-button" title="">
-            <Dropdown.Item href="/Signin">Signin</Dropdown.Item>
-            <Dropdown.Item href="/Signup">Register</Dropdown.Item>
-          </DropdownButton>
-        </Row>
-      </Container>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Tinker</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Quiz">Quiz</Nav.Link>
+            <Nav.Link href="/Challenges">Challenges</Nav.Link>
+            <NavDropdown title="Add" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/AddChallenge">
+                Add Challenge
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/AddResources">
+                Add Resources
+              </NavDropdown.Item>
+              <NavDropdown.Item href="AddAssignment">
+                Add Assignment
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <Button variant="outline-success" href="/Signin">
+              Signin
+            </Button>
+            <Button variant="outline-success" href="/Signup">
+              Signup
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 };
