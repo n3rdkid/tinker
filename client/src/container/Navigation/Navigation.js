bb0cb061fe1969fed7c38ba216bf89b80ff113be
Saurav Adhikari <sauravads123@gmail.com>
2019-05-27 22:05:36 +0545

    Setup Conditional Navlink

--- client/src/container/Navigation/Navigation.js
+++ client/src/container/Navigation/Navigation.js
@@ -4,42 +4,67 @@ import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 
-const Navigation = () => {
-  return (
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">Tinker</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/Quiz">Quiz</Nav.Link>
-            <Nav.Link href="/Challenges">Challenges</Nav.Link>
-            <NavDropdown title="Add" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/AddChallenge">
-                Add Challenge
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/AddResources">
-                Add Resources
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/Assignment">
-                Add Assignment
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <Button variant="outline-success" href="/Signin">
-              Signin
-            </Button>
-            <Button variant="outline-success" href="/Signup">
-              Signup
-            </Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
-};
+class Navigation extends React.Component {
+  onLogoutClicked = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+  render() {
+    const { isAuthenticated } = this.props.auth;
+    const authLinks = (
+      <Form inline>
+        <Button onClick={this.onLogoutClicked} variant="outline-success">
+          Logout
+        </Button>
+      </Form>
+    );
+    const guestLinks = (
+      <Form inline>
+        <Button variant="outline-success" href="/Signin">
+          Signin
+        </Button>
+        <Button variant="outline-success" href="/Signup">
+          Signup
+        </Button>
+      </Form>
+    );
 
-export default Navigation;
+    return (
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">Tinker</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/Quiz">Quiz</Nav.Link>
+              <Nav.Link href="/Challenges">Challenges</Nav.Link>
+              <NavDropdown title="Add" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/AddChallenge">
+                  Add Challenge
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/AddResources">
+                  Add Resources
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/Assignment">
+                  Add Assignment
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            {isAuthenticated ? authLinks : guestLinks}
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Navigation);
