9fae1ac3bf0f265d70bf6a34f83a62152ff914c9
beevekmgr <beevekmgr@gmail.com>
2019-05-30 21:12:29 +0545

    Minor bug fix on QUiz

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,9 @@
-import { GET_ERRORS,SET_CURRENT_USER } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 import axios from "axios";
-import setAuthToken from '../utils/setAuthToken';
+import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
 export const registerUser = (userData, history) => dispatch => {
-  console.log(userData)
+  console.log(userData);
   axios
     .post("http://localhost:5000/api/users/register", userData)
     .then(() => history.push("/challenges"))
@@ -12,41 +12,41 @@ export const registerUser = (userData, history) => dispatch => {
         type: GET_ERRORS,
         payload: err.response.data
       })
-    );   
+    );
 };
 
 //Login Get user token
 export const loginUser = userData => dispatch => {
-    axios.post("http://localhost:5000/api/users/signin",userData)
-    .then(res=>{
-        //Save to localstorage
-        const {token} =res.data;
-        //Set token to localStorage
-        localStorage.setItem('jwtToken',token);
-        //Set token to auth header
-        setAuthToken(token);
-        //Decode token
-        const decoded=jwt_decode(token);
-        //Set current user
-        dispatch(setCurrentUser(decoded))
-
-    }).catch(err=>dispatch({type:GET_ERRORS,payload:err.response.data}));
+  axios
+    .post("http://localhost:5000/api/users/signin", userData)
+    .then(res => {
+      //Save to localstorage
+      const { token } = res.data;
+      //Set token to localStorage
+      localStorage.setItem("jwtToken", token);
+      //Set token to auth header
+      setAuthToken(token);
+      //Decode token
+      const decoded = jwt_decode(token);
+      //Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
 };
 //set logged in user
-export const setCurrentUser=decoded=>{
-    return {
-        type:SET_CURRENT_USER,
-        payload:decoded
-    }
-}
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
 
 //Logout user
-export const logoutUser=()=>dispatch=>{
-    //Remove token from localStorage
-    localStorage.removeItem('jwtToken');
-    //Remove auth for future headers
-    setAuthToken(false);
-    //Set current user to {} and authenticated to false
-    dispatch(setCurrentUser({}))
-
-}
+export const logoutUser = () => dispatch => {
+  //Remove token from localStorage
+  localStorage.removeItem("jwtToken");
+  //Remove auth for future headers
+  setAuthToken(false);
+  //Set current user to {} and authenticated to false
+  dispatch(setCurrentUser({}));
+};
