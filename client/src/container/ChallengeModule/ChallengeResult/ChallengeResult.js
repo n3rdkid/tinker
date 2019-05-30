ca2df616665eb6a0cbf40f989576e1ce30f040ea
Saurav Adhikari <sauravads123@gmail.com>
2019-05-30 22:52:57 +0545

    Implemented time in leaderboard

--- client/src/container/ChallengeModule/ChallengeResult/ChallengeResult.js
+++ client/src/container/ChallengeModule/ChallengeResult/ChallengeResult.js
@@ -8,39 +8,42 @@ class ChallengeResult extends React.Component {
   state = {
     timeTaken: this.props.location.state.timeTaken,
     questionId: this.props.location.state.questionId,
-    leaderboard:null
+    leaderboard: null
   };
 
-  componentDidMount() {
-    console.log(this.props.location);
-    axios
-      .get(`http://localhost:5000/api/challenges/leaderboard/${this.state.questionId}`)
-      .then(response =>this.setState({leaderboard:response.data}))
+  async componentDidMount() {
+    await axios
+      .get(
+        `http://localhost:5000/api/challenges/leaderboard/${this.state.questionId
+        }`
+      )
+      .then(response => this.setState({ leaderboard: response.data }))
       .catch(error => console.log(error));
-    console.log(this.state.leaderboard)
-    }
+  }
 
   render() {
-    let leaderboardList=[]
-      
-      if(this.state.leaderboard)
-      {
-         
-          this.state.leaderboard.forEach((submission,index)=>
-            {
-                let item=<li>ID {index+1} Time Taken {submission.timeTaken} Date {submission.submisison_date}</li>
-                leaderboardList.push(item)
-            })
+    let leaderboardList = [];
 
-      }else
-      {
-        leaderboardList=<Spinner/>
-      }
+    if (this.state.leaderboard) {
+      this.state.leaderboard.forEach((submission, index) => {
+        let time = submission.submisison_date.split("T");
+        console.log(time)
+        let item = (
+          <li>
+            ID ::{index + 1} Time Taken:: {submission.timeTaken} Date::{time[0]}
+            {/* {formattedDate} */}
+          </li>
+        );
+        leaderboardList.push(item);
+      });
+    } else {
+      leaderboardList = <Spinner />;
+    }
     return (
       <>
         <div>You completed in {this.state.timeTaken} ms</div>
         {leaderboardList}
-     </>
+      </>
     );
   }
 }
