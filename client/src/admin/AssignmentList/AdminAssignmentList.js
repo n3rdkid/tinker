c8833ab940e70dfa9f45087f81d3fd5bbfee2280
Saurav Adhikari <sauravads123@gmail.com>
2019-06-09 14:48:17 +0545

    Improv : Add Pagination to add assignment list

--- client/src/admin/AssignmentList/AdminAssignmentList.js
+++ client/src/admin/AssignmentList/AdminAssignmentList.js
@@ -7,17 +7,39 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { connect } from "react-redux";
 import Col from "react-bootstrap/Col";
 import Spinner from "../../UI/Spinner/Spinner";
-import "./AssignmentItem.css"
+import "./AssignmentItem.css";
+import { MDBDataTable } from "mdbreact";
+const dataTableData = {
+  columns: [
+    {
+      label: "S.No",
+      field: "assignment_id",
+      sort: "asc"
+    },
+    {
+      label: "Due Date",
+      field: "dueDate",
+      sort: "asc"
+    },
+    {
+      label: "Action",
+      field: "view",
+      sort: "asc"
+    }
+  ],
+  rows: []
+};
 class AdminAssignmentList extends React.Component {
   state = {
-    data: ""
+    data: "",
+    loading:true
   };
-  componentDidMount=()=>{
-    console.log("Inside AdminAssignmentsLists ",this.props)
-    if(this.props.auth.user.role!=="teacher")
-    this.props.history.push("/restricted")
-    console.log("Inside AssignmentItem")
-    axios
+
+  componentDidMount = async () => {
+    console.log("Inside AdminAssignmentsLists ", this.props);
+    if (this.props.auth.user.role !== "teacher")
+      this.props.history.push("/restricted");
+    await axios
       .get("http://localhost:5000/api/admin/assignments")
       .then(response => {
         this.setState({
@@ -25,37 +47,46 @@ class AdminAssignmentList extends React.Component {
         });
       })
       .catch(error => console.log(error));
-      console.log("Inside Assignment Item",this.state.data)
-  }
+
+    let assignmentData = this.state.data;
+    dataTableData.rows=[];
+    for (let assignment of assignmentData) {
+      dataTableData.rows.push({
+        assignmentId: assignment.id,
+        dueDate: assignment.dueDate.split("T")[0],
+        view: (
+          <button
+            id={assignment.id}
+            onClick={this.clickHandler}
+            className="ml-auto btn btn-success"
+          >
+            View Submissions
+          </button>
+        )
+      });
+    }
+    this.setState({loading:false});
+  };
+
   clickHandler = e => {
+    console.log("View id ",e.target.id)
     let id = e.target.id || e.target.parentElement.id;
     this.props.history.push(`/admin/assignments/${id}`);
   };
-  addHandler=()=>
-  {
+  addHandler = () => {
     this.props.history.push("/admin/add");
-  }
+  };
 
   render() {
-    let container=<p>No assignments yet</p>;
-    if (this.state.data === "") container = <Spinner />;
+    let container = <p>No assignments yet</p>;
+    if (this.state.loading) container = <Spinner />;
     else {
-      let assignmentData = this.state.data;
-      let assignmentList = [];
-      for (let assignment of assignmentData) {
-        assignmentList.push(
-          <div 
-            className="d-flex container assignmentItem"
-            id={assignment.id}
-            onClick={this.clickHandler}
-          >
-            <span>Assignment No : {assignment.id} </span>
-            <span className="mx-auto">Due : {assignment.dueDate.split("T")[0]} </span>
-            <button className="ml-auto btn btn-success">View Submissions</button>
-          </div>
-        );
-      }
-      container = assignmentList;
+      container =  <MDBDataTable
+      striped
+      hover
+      data={dataTableData}
+      searching={true}
+    />;
     }
     return (
       <div>
@@ -63,12 +94,16 @@ class AdminAssignmentList extends React.Component {
           {/*Need to add Heatmap ->*/}
           <Row>
             <Col xs={3}>
-            <Card>
-            <button class="btn btn-primary" onClick={this.addHandler}>Add Assignment</button>
+              <Card>
+                <button class="btn btn-primary" onClick={this.addHandler}>
+                  Add Assignment
+                </button>
               </Card>
             </Col>
             <Col xs={1}> </Col>
-            <Col xs={8}> {container}</Col>
+            <Col xs={8}>
+              {container}
+            </Col>
           </Row>
         </Container>
       </div>
