32c0b1bb25d44d5384c50d67506db83b97ec928a
Saurav Adhikari <sauravads123@gmail.com>
2019-06-10 20:21:50 +0545

    style : sign up page

--- client/src/UI/SignUp/SignUp.js
+++ client/src/UI/SignUp/SignUp.js
@@ -8,6 +8,7 @@ import classnames from "classnames";
 // import propTypes from "propTypes";
 import { registerUser } from "../../actions/authActions";
 import { withRouter } from "react-router-dom";
+import { Card } from "react-bootstrap";
 class SignUp extends React.Component {
   state = {
     username: "",
@@ -41,79 +42,86 @@ class SignUp extends React.Component {
     const { errors } = this.state;
     const { user } = this.props.auth;
     return (
-      <div className="card col-md-6 mx-auto my-auto">
-        <Form>
-          <Form.Group as={Row}>
-            <Form.Label column sm={3}>
-              Email
-            </Form.Label>
-            <Col sm={9}>
-              <Form.Control
-                className={classnames({ "is-invalid": errors.email })}
-                name="email"
-                onChange={e => this.changeHandler(e)}
-                value={this.state.email}
-                type="email"
-                placeholder="Email"
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.email}
-              </Form.Control.Feedback>
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} controlId="formHorizontalEmail">
-            <Form.Label column sm={3}>
-              Username
-            </Form.Label>
-            <Col sm={9}>
-              <Form.Control
-                name="username"
-                className={classnames({ "is-invalid": errors.username })}
-                type="text"
-                placeholder="UserName"
-                onChange={e => this.changeHandler(e)}
-                value={this.state.username}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.username}
-              </Form.Control.Feedback>
-            </Col>
-          </Form.Group>
+      <Card.Body>
+        <Row className="justify-content-center align-items-center">
+          <Col md="6">
+            <Card>
+              <Card.Header> Sign up </Card.Header>
+              <Card.Body>
+                <Form>
+                  <Form.Group as={Row}>
+                    <Form.Label column sm={3}>
+                      Email
+                    </Form.Label>
+                    <Col sm={9}>
+                      <Form.Control
+                        className={classnames({ "is-invalid": errors.email })}
+                        name="email"
+                        onChange={e => this.changeHandler(e)}
+                        value={this.state.email}
+                        type="email"
+                        placeholder="Email"
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {errors.email}
+                      </Form.Control.Feedback>
+                    </Col>
+                  </Form.Group>
+                  <Form.Group as={Row} controlId="formHorizontalEmail">
+                    <Form.Label column sm={3}>
+                      Username
+                    </Form.Label>
+                    <Col sm={9}>
+                      <Form.Control
+                        name="username"
+                        className={classnames({
+                          "is-invalid": errors.username
+                        })}
+                        type="text"
+                        placeholder="Username"
+                        onChange={e => this.changeHandler(e)}
+                        value={this.state.username}
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {errors.username}
+                      </Form.Control.Feedback>
+                    </Col>
+                  </Form.Group>
 
-          <Form.Group as={Row} controlId="formHorizontalPassword">
-            <Form.Label column sm={3}>
-              Password
-            </Form.Label>
-            <Col sm={9}>
-              <Form.Control
-                className={classnames({ "is-invalid": errors.password })}
-                name="user_password"
-                type="password"
-                placeholder="Password"
-                onChange={e => this.changeHandler(e)}
-                value={this.state.user_password}
-              />
-               <Form.Control.Feedback type="invalid">
-                {errors.password}
-              </Form.Control.Feedback>
-            </Col>
-          </Form.Group>
+                  <Form.Group as={Row} controlId="formHorizontalPassword">
+                    <Form.Label column sm={3}>
+                      Password
+                    </Form.Label>
+                    <Col sm={9}>
+                      <Form.Control
+                        className={classnames({
+                          "is-invalid": errors.password
+                        })}
+                        name="user_password"
+                        type="password"
+                        placeholder="Password"
+                        onChange={e => this.changeHandler(e)}
+                        value={this.state.user_password}
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {errors.password}
+                      </Form.Control.Feedback>
+                    </Col>
+                  </Form.Group>
 
-          <Form.Group as={Row} controlId="formHorizontalCheck">
-            <Col sm={{ span: 10, offset: 2 }}>
-              <Form.Check label="Remember me" />
-            </Col>
-          </Form.Group>
-
-          <Form.Group as={Row}>
-            <Col sm={{ span: 10, offset: 2 }}>
-              <Button type="submit" onClick={e => this.onSubmit(e)}>
-                Register
-              </Button>
-            </Col>
-          </Form.Group>
-        </Form>
-      </div>
+                  <Form.Group as={Row}>
+                    <Col>
+                      <Button type="submit" onClick={e => this.onSubmit(e)}>
+                        Register
+                      </Button>
+                    </Col>
+                  </Form.Group>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Card.Body>
     );
   }
 }
