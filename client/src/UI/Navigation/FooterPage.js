2f463e83c72c0be709f19515bfd708291751a8a0
beevekmgr <beevekmgr@gmail.com>
2019-06-11 14:27:26 +0545

    Footer position recoreected

--- client/src/UI/Navigation/FooterPage.js
+++ client/src/UI/Navigation/FooterPage.js
@@ -3,6 +3,7 @@ import { Flag, Grid, Segment } from "semantic-ui-react";
 import "./FooterPage.css";
 const FooterPage = () => {
   return (
+<<<<<<< Updated upstream
     // <div class="footer">
     //   <Segment
     //     color="violet"
@@ -21,6 +22,25 @@ const FooterPage = () => {
     //   </Segment>
     // </div>
     <></>
+=======
+    <div class="footer">
+      <Segment
+        color="violet"
+        attached="bottom"
+        textAlign="center"
+        style={{ padding: "0.5em 0em" }}
+      >
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={6} />
+            <Flag name="nepal" />
+            &copy; {new Date().getFullYear()} Copyright:{" "}
+            <a href="https://www.tinker.com"> TINKER.COM </a>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </div>
+>>>>>>> Stashed changes
   );
 };
 
