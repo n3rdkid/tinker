b9fc9f1603e2a42de8330546d4217c97100f7cb4
beevekmgr <beevekmgr@gmail.com>
2019-06-14 17:01:08 +0545

    UserGuide update

--- client/src/userguide/UserGuide.js
+++ client/src/userguide/UserGuide.js
@@ -1,8 +1,44 @@
 import React from "react";
+import {
+  Button,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Search,
+  Segment
+} from "semantic-ui-react";
 
 class UserGuide extends React.Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <Segment placeholder>
+        <Grid columns={3} relaxed="very" textAlign="center">
+          <Grid.Row verticalAlign="middle">
+            <Grid.Column>
+              <Header icon>
+                <Icon name="bolt" />
+              </Header>
+              <Button primary>Go to Quiz</Button>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Header icon>
+                <Icon name="chess knight" />
+              </Header>
+              <Button primary>Go to Challenge</Button>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Header icon>
+                <Icon name="write" />
+              </Header>
+              <Button primary>Go to Assignment</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    );
   }
 }
 export default UserGuide;
